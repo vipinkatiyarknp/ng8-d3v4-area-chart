633c3755de131b7e123aed4300ba3b58ad8f8701
Vipin Kumar <vipikum3@cisco.com>
2019-06-21 16:29:53 +0530

    Version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component , ViewEncapsulation} from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,120 @@ import { Component , ViewEncapsulation} from '@angular/core';
 })
 export class AppComponent {
   d3v4AreaChartConfig = {
+    svgBg: '#F5F5F5',
+    dotBg: '#333',
+    dotColor: '#333',
+    lineColor: '#000',
+    linewidth: '1',
+    areaBg: '#CAFACC',
+    showTooltip: true,
+    tooltipBg: '#FFF',
+    tooltipColor: '#666'
   };
-  d3v4AreaChartData = [];
+  d3v4AreaChartData = [
+    {
+      date: '1-May-12',
+      close: 58.13
+    },
+    {
+      date: '30-Apr-12',
+      close: 10.98
+    },
+    {
+      date: '27-Apr-12',
+      close: 67
+    },
+    {
+      date: '26-Apr-12',
+      close: 15.7
+    },
+    {
+      date: '25-Apr-12',
+      close: 99
+    },
+    {
+      date: '24-Apr-12',
+      close: 30.28
+    },
+    {
+      date: '23-Apr-12',
+      close: 66.7
+    },
+    {
+      date: '20-Apr-12',
+      close: 134.98
+    },
+    {
+      date: '19-Apr-12',
+      close: 45.44
+    },
+    {
+      date: '18-Apr-12',
+      close: 143.34
+    },
+    {
+      date: '17-Apr-12',
+      close: 543.7
+    },
+    {
+      date: '16-Apr-12',
+      close: 580.13
+    },
+    {
+      date: '13-Apr-12',
+      close: 605.23
+    },
+    {
+      date: '12-Apr-12',
+      close: 622.77
+    },
+    {
+      date: '11-Apr-12',
+      close: 626.2
+    },
+    {
+      date: '10-Apr-12',
+      close: 228.44
+    },
+    {
+      date: '9-Apr-12',
+      close: 136.23
+    },
+    {
+      date: '5-Apr-12',
+      close: 233.68
+    },
+    {
+      date: '4-Apr-12',
+      close: 624.31
+    },
+    {
+      date: '3-Apr-12',
+      close: 129.32
+    },
+    {
+      date: '2-Apr-12',
+      close: 218.63
+    },
+    {
+      date: '30-Mar-12',
+      close: 199.55
+    },
+    {
+      date: '29-Mar-12',
+      close: 609.86
+    },
+    {
+      date: '28-Mar-12',
+      close: 417.62
+    },
+    {
+      date: '27-Mar-12',
+      close: 614.48
+    },
+    {
+      date: '26-Mar-12',
+      close: 406.98
+    }
+  ];
 }
--- src/app/d3v4-area-chart/d3v4-area-chart.component.ts
+++ src/app/d3v4-area-chart/d3v4-area-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-d3v4-area-chart',
@@ -8,8 +9,144 @@ import { Component, OnInit } from '@angular/core';
 export class D3v4AreaChartComponent implements OnInit {
 
   constructor() { }
+  inputChartData: any;
+  inputChartConfig: any;
+
+  @Input('d3v4AreaChartData')
+  set d3v4AreaChartData(val) {
+    this.inputChartData = val;
+  }
+  get d3v4AreaChartData() {
+    return this.inputChartData;
+  }
+
+
+  @Input('d3v4AreaChartConfig')
+  set d3v4AreaChartConfig(val) {
+    this.inputChartConfig = val;
+  }
+  get d3v4AreaChartConfig() {
+    return this.inputChartConfig;
+  }
+
 
   ngOnInit() {
+    this.updateChart();
+  }
+
+  updateChart() {
+    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    const width = 960 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    // parse the date / time
+    const parseTime = d3.timeParse('%d-%b-%y');
+
+    // set the ranges
+    const x = d3.scaleTime().range([0, width]);
+    const y = d3.scaleLinear().range([height, 0]);
+
+    // define the area
+    const area = d3.area()
+      .x(d => {
+        return x(d.date);
+      })
+      .y0(height)
+      .y1(d => {
+        return y(d.close);
+      });
+
+    // define the line
+    const valueline = d3.line()
+      .x(d => {
+        return x(d.date);
+      })
+      .y(d => {
+        return y(d.close);
+      });
+
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+
+    const svg = d3.select('#chart-container').append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .attr('background-color', this.inputChartConfig.svgBg)
+      .append('g')
+      .attr('transform',
+        'translate(' + margin.left + ',' + margin.top + ')');
+
+    // get the data
+    if (this.inputChartData) {
+      const data = this.inputChartData;
+      // format the data
+      data.forEach(d => {
+        d.date = parseTime(d.date);
+        d.close = +d.close;
+      });
+
+      // scale the range of the data
+      x.domain(d3.extent(data, d => {
+        return d.date;
+      }));
+      y.domain([0, d3.max(data, d => {
+        return d.close;
+      })]);
+
+      // add the area
+      svg.append('path')
+        .data([data])
+        .attr('fill', this.inputChartConfig.areaBg)
+        .attr('d', area);
+
+      // add the valueline path.
+      svg.append('path')
+        .data([data])
+        .attr('fill', 'none')
+        .attr('stroke', this.inputChartConfig.lineColor)
+        .attr('stroke-width', this.inputChartConfig.linewidth + 'px')
+        .attr('d', valueline);
+
+      const div = d3.select('body').append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0);
+
+      svg.selectAll('myCircles')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('fill', this.inputChartConfig.dotBg)
+        .attr('stroke', this.inputChartConfig.dotColor)
+        .attr('cx', d => {
+          return x(d.date);
+        })
+        .attr('cy', d => {
+          return y(d.close);
+        })
+        .attr('r', 6).on('mouseover', d => {
+          div.transition()
+            .duration(200)
+            .style('opacity', .9);
+          div.html(parseTime(d.date) + '<br/>' + d.close)
+            .style('left', (d3.event.pageX) + 'px')
+            .style('top', (d3.event.pageY - 28) + 'px');
+        })
+        .on('mouseout', d => {
+          div.transition()
+            .duration(500)
+            .style('opacity', 0);
+        });
+
+      // add the X Axis
+      svg.append('g')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(d3.axisBottom(x));
+
+      // add the Y Axis
+      svg.append('g')
+        .call(d3.axisLeft(y));
+    }
   }
 
 }
